a9c6b1f81f53a85dac68d3caa947031d73d8abe8
zdy <969251532@qq.com>
2025-12-22 12:33:42 +0800

    1

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import ArticleView from '@/views/ArticleView.vue'
+import LayoutView from '@/views/LayoutView.vue'
+import LoginView from '@/views/LoginView.vue'
+import RegisterView from '@/views/RegisterView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/login',
+      component: LoginView,
+    },
+    {
+      path: '/register',
+      component: RegisterView,
+    },
+    {
+      path: '/',
+      component: LayoutView,
+    },
+    {
+      path: '/article/:id',
+      component: ArticleView,
+    },
+  ],
 })
 
 export default router
---
+++ src/views/LayoutView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="layout-page">
+    <van-tabbar>
+      <van-tabber-item icon="notes-o">面经</van-tabber-item>
+      <van-tabber-item icon="star-o">收藏</van-tabber-item>
+      <van-tabber-item icon="like-o">喜欢</van-tabber-item>
+      <van-tabber-item icon="user-o">我的</van-tabber-item>
+    </van-tabbar>
+  </div>
+</template>
