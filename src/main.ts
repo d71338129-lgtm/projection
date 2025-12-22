f8185b6359b48f608fcf1c1881a82765b6bc614a
zdy <969251532@qq.com>
2025-12-22 11:28:06 +0800

    11

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import vant from 'vant'
+import 'vant/lib/index.css'
 import App from './App.vue'
 import router from './router'
 
@@ -8,5 +9,5 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(vant)
 app.mount('#app')
